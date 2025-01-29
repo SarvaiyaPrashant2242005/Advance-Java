efc23a174e30397cad0a88980a8457e7ac5515f5
SarvaiyaPrashant2242005 <115221462+SarvaiyaPrashant2242005@users.noreply.github.com>
2025-01-29 21:06:18 +0530

    notes

---
+++ Projects/Second_app/src/main/java/Servlet_test.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/prash")
+public class Servlet_test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Hi, Ritesh");
+        System.out.println("from doGET");
+    }
+}
